c2753bc8077dd4771d45172c4d7d86aa39077390
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2025-08-20 17:51:42 +0530

    Updated configs and removed secrets

--- backend/void-url-shortner/src/main/java/com/example/void_url_shortner/configuration/S3Configuration.java
+++ backend/void-url-shortner/src/main/java/com/example/void_url_shortner/configuration/S3Configuration.java
@@ -3,29 +3,19 @@ package com.example.void_url_shortner.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Configuration {
-    @Value("${aws.credentials.profile-name}")
-    private String awsCredentialsProfileName;
-
     @Value("${aws.region}")
     private String awsRegion;
 
     @Bean
-    public ProfileCredentialsProvider awsCredentialsProvider() {
-        return ProfileCredentialsProvider.create(awsCredentialsProfileName);
-    }
-
-    @Bean
-    public S3Presigner s3Presigner(ProfileCredentialsProvider credentialsProvider) {
+    public S3Presigner s3Presigner() {
         return S3Presigner
                 .builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
